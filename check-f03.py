4b9dccc48cd8f4605987ccb03639be88412ea42a
Ben Andre <bjandre@gmail.com>
2014-12-30 16:25:03 -0700

    misc hacking on whitespace rules and keywords to get basic bare module to run through parser. add test input and driver to check parser against test files.

---
+++ check-f03.py
@@ -0,0 +1,57 @@
+from fortran_2003 import fortran_2003Parser
+
+
+def main(filename, startrule, trace=False, whitespace=None, nameguard=None):
+    import json
+    with open(filename) as f:
+        text = f.read()
+    parser = fortran_2003Parser(parseinfo=False, ignorecase=True, whitespace="") #, eol_comments_re="!.*?")
+    ast = parser.parse(
+        text,
+        startrule,
+        filename=filename,
+        trace=trace,
+        whitespace=whitespace,
+        nameguard=nameguard)
+    print('AST:')
+    print(ast)
+    print()
+    print('JSON:')
+    print(json.dumps(ast, indent=2))
+    print()
+
+if __name__ == '__main__':
+    import argparse
+    import string
+    import sys
+
+    class ListRules(argparse.Action):
+        def __call__(self, parser, namespace, values, option_string):
+            print('Rules:')
+            for r in fortran_2003Parser.rule_list():
+                print(r)
+            print()
+            sys.exit(0)
+
+    parser = argparse.ArgumentParser(description="Simple parser for fortran_2003.")
+    parser.add_argument('-l', '--list', action=ListRules, nargs=0,
+                        help="list all rules and exit")
+    parser.add_argument('-n', '--no-nameguard', action='store_true',
+                        dest='no_nameguard',
+                        help="disable the 'nameguard' feature")
+    parser.add_argument('-t', '--trace', action='store_true',
+                        help="output trace information")
+    parser.add_argument('-w', '--whitespace', type=str, default=string.whitespace,
+                        help="whitespace specification")
+    parser.add_argument('file', metavar="FILE", help="the input file to parse")
+    parser.add_argument('startrule', metavar="STARTRULE",
+                        help="the start rule for parsing")
+    args = parser.parse_args()
+
+    main(
+        args.file,
+        args.startrule,
+        trace=args.trace,
+        whitespace=args.whitespace,
+        nameguard=not args.no_nameguard
+    )
