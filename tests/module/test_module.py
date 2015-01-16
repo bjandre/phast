6ddcb5657b6f551a73f0150d6e403c8b1bf012c8
Ben Andre <bjandre@gmail.com>
2015-01-16 10:48:37 -0700

    tests: add python unit tests and make rules to get "make test" working.

---
+++ tests/module/test_module.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+
+import unittest
+from phast.fortran_2003 import fortran_2003Parser
+
+class phast_module(unittest.TestCase):
+    """unittest suite for phast's ability to process fortran 2003 modules
+    """
+
+    def setUp(self):
+        self.whitespace = " \t"
+        self.startrule = "module"
+        self.parser = fortran_2003Parser(parseinfo=False, ignorecase=True, trace_length=512)
+        self.text = None
+        self.ast = None
+        self.test_dir = "module"
+
+    def tearDown(self):
+        pass
+
+    def generate_ast(self, filename):
+        #import os.path
+        #self.assertTrue(os.path.exists(filename))
+        ast = None
+        try:
+            with open(filename) as f:
+                self.text = f.read()
+        except Exception:
+            pass
+        else:
+            ast = self.parser.parse(self.text, self.startrule, filename, whitespace=self.whitespace)
+        return ast
+
+    def test_bad_filename(self):
+        """Test that the generate_ast function returns None for a bad filename
+        """
+        filename = "bad-filename"
+        ast = self.generate_ast(filename)
+        self.assertIsNone(ast)
+
+    def test_0001(self):
+        filename = "{0}/t0001_minimal_module.F03".format(self.test_dir)
+        ast = self.generate_ast(filename)
+        self.assertIsNotNone(ast)
+
+    def test_0002(self):
+        filename = "{0}/t0002_minimal_module_comments.F03".format(self.test_dir)
+        ast = self.generate_ast(filename)
+        self.assertIsNotNone(ast, msg=ast.__repr__())
+
+    def test_0003(self):
+        """
+        """
+        filename = "{0}/t0003_module_param.F03".format(self.test_dir)
+        ast = self.generate_ast(filename)
+        self.assertIsNotNone(ast)
+
+    def test_0004(self):
+        """
+        """
+        filename = "{0}/t0004_module_use.F03".format(self.test_dir)
+        ast = self.generate_ast(filename)
+        self.assertIsNotNone(ast)
+
+    def test_0005(self):
+        """
+        """
+        filename = "{0}/t0005_module_implicit_none.F03".format(self.test_dir)
+        ast = self.generate_ast(filename)
+        self.assertIsNotNone(ast)
+
+    def test_0006(self):
+        """
+        """
+        filename = "{0}/t0006_module_dummy_args.F03".format(self.test_dir)
+        ast = self.generate_ast(filename)
+        self.assertIsNotNone(ast)
+
+    def test_0007(self):
+        """Test that the generate_ast function returns none when the parser fails.
+        """
+        filename = "{0}/t0007_minimal_module_fail.F03".format(self.test_dir)
+        self.assertRaises(Exception, self.generate_ast, filename)
+
+
+
+if __name__ == "__main__":
+    unittest.main()
